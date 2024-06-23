f35520ef3043ad1d53c70964b57bd1e74b46ee7c
Vidit Jain <84613629+ViditJain123@users.noreply.github.com>
2024-06-23 11:47:31 +0530

    User Schema Completed

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,40 +1,72 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, Mongoose } from "mongoose";
 
 export interface User extends Document {
-  username: string;
   email: string;
+  phoneNumber: number;
   password: string;
-  verifyCode: string;
-  verifyCodeExpire: Date;
+  verificationToken: string;
+  verifyTokenExpire: Date;
   verified: boolean;
-  orderHistory: Number[];
-  cart: string[];
+  orderHistory: mongoose.Schema.Types.ObjectId[];
+  cart: Cart[];
   role: "user" | "admin";
 }
 
-const UserSchema: Schema<User> = new Schema({
-  username: {
-    type: String,
-    required: [true, "Username is required"],
-    trim: true,
-    unique: true,
+export interface Cart extends Document{
+  userId: mongoose.Schema.Types.ObjectId;
+  product: mongoose.Schema.Types.ObjectId;
+  quantity: number;
+}
+
+const CartSchema: Schema<Cart> = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
   },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  }
+})
+
+const UserSchema: Schema<User> = new Schema({
   email: {
     type: String,
     required: [true, "Email is required"],
     unique: true,
     match: [/\S+@\S+\.\S+/, "Please enter a valid email"],
   },
-  password: { type: String, required: [true, "password is required"] },
-  verifyCode: { type: String, required: [true, "verifyCode is required"] },
-  verifyCodeExpire: {
-    type: Date,
-    required: [true, "verifyCodeExpire is required"],
+  phoneNumber: {
+    type: Number,
+    required: true,
+    minlength: 10,
+    maxlength: 10,
   },
+  password: { type: String, required: [true, "password is required"] },
   verified: { type: Boolean, default: false },
-  orderHistory: { type: [Number], required: true },
-  cart: { type: [String], required: true },
-  role: { type: String, required: true, enum: ["user", "admin"] },
+  verificationToken: {
+    type: String,
+    required: [true, "verifyCode is required"],
+  },
+  verifyTokenExpire: {
+    type: Date,
+    required: [true, "verifyTokenExpire is required"],
+  },
+  orderHistory: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "Order",
+    required: true,
+  },
+  cart: {
+    type: [CartSchema],
+    required: true
+  },
 });
 
 const UserModel =
