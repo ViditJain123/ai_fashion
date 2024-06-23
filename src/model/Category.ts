26373969c5b5c3b381e0688f18ee306d07811d5f
Vidit Jain <84613629+ViditJain123@users.noreply.github.com>
2024-06-23 14:38:52 +0530

    Category Model added

--- src/model/Category.ts
+++ src/model/Category.ts
@@ -1,19 +1,23 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, Model, mongo } from "mongoose";
 
 export interface Category extends Document {
-  name: string;
-  description: string;
+    categoryName: String;
+    categoryDescription: String;
 }
 
 const CategorySchema: Schema<Category> = new Schema({
-  name: { type: String, required: [true, "Category name is required"] },
-  description: {
-    type: String,
-    required: [true, "Category description is required"],
-  },
-});
+    categoryName: {
+        type: String,
+        required: [true, "Category Name is required"],
+    },
+    categoryDescription: {
+        type: String,
+        required: [true, "Category Description is required"],
+    },
+})
 
 const CategoryModel =
-  (mongoose.models.Category as mongoose.Model<Category>) ||
-  mongoose.model<Category>("Category", CategorySchema);
+    (mongoose.models.Category as mongoose.Model<Category>) ||
+    mongoose.model<Category>("Category", CategorySchema);
+
 module.exports = CategoryModel;
